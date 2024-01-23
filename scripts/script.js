d014c910267b782bf78da0b7953c62e8d31e0435
skyy_dev <skyzeduardo@gmail.com>
2024-01-23 17:17:20 -0300

    otimização de imagens e responsividade

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,71 @@
+document.addEventListener('DOMContentLoaded', function sld() {
+  // Inicializar Flickity
+  var flkty = new Flickity('.hero-slider', {
+      wrapAround: true,
+      autoPlay: 6000, // Mudar slide a cada 15 segundos
+      pauseAutoPlayOnHover: false, // Não pausar no hover
+      prevNextButtons: true, // Desabilitar botões de navegação
+  });
+});
+
   function moverFoto() {
+    var foto = document.getElementById('userpic');
+    foto.style.transform = 'translate(0%, 0)'
+    foto.style.left = '0%';
+  }
+setTimeout(moverFoto, 300);
+  function moverFotoDesk() {
     var foto = document.getElementById('userpic');
     foto.style.transform = 'translate(-10%, 0)'
     foto.style.left = '-10%';
+    foto.style.marginRight = '150px'
   }
-  setTimeout(moverFoto, 1000);
 
   function moverTexto() {
     var txtmove = document.getElementById('animatedtxt');
-    txtmove.style.transform = 'translate(-20%, 0)'
-    txtmove.style.left = '-20%';
+    txtmove.style.transform = 'translate(-10%, 0)'
+    txtmove.style.left = '-10%';
   }
-  setTimeout(moverTexto, 1000);
-const options = {
-  accessibility: true,
-  prevNextButtons: true,
-  pageDots: true,
-  setGallerySize: false,
-  arrowShape: {
-      x0: 1,
-      x1: 58,
-      y1: 62,
-      x2: 55,
-      y2: 48,
-      x3: 18
+  function verificarMediaQuery() {
+    if (window.matchMedia('(min-width: 600px)').matches) {
+        moverFoto();
+        sld();
+        // Verifica se a largura da tela é maior ou igual a 600 pixels (exemplo)
+    } if (window.matchMedia('(min-width: 1200px)').matches) {
+        moverTexto();
+        moverFotoDesk();
+    }
   }
-};
+verificarMediaQuery();
 
-// Function to set background position forr slides
-function setBgPosition(slide, index) {
-  const x = -(slide.target + flkty.x) / 3;
-  slides[index].style.backgroundPosition = `${x}px`;
-}
 
-// Slides initialization
-const carousel = document.querySelector('[carousel]');
-const slides = Array.from(document.getElementsByClassName('carousel-cell'));
-const flkty = new Flickity(carousel, options);
+// const options = {
+//   accessibility: true,
+//   prevNextButtons: true,
+//   pageDots: true,
+//   setGallerySize: false,
+//   arrowShape: {
+//       x0: 1,
+//       x1: 58,
+//       y1: 62,
+//       x2: 55,
+//       y2: 48,
+//       x3: 18
+//   }
+// };
 
-// Event listener using bg position
-flkty.on('scroll', () => {
-  flkty.slides.forEach(setBgPosition);
-});
+// // Function to set background position forr slides
+// function setBgPosition(slide, index) {
+//   const x = -(slide.target + flkty.x) / 3;
+//   slides[index].style.backgroundPosition = `${x}px`;
+// }
+
+// // Slides initialization
+// const carousel = document.querySelector('[carousel]');
+// const slides = Array.from(document.getElementsByClassName('carousel-cell'));
+// const flkty = new Flickity(carousel, options);
+
+// // Event listener using bg position
+// flkty.on('scroll', () => {
+//   flkty.slides.forEach(setBgPosition);
+// });
